b497f5df440b70bb7b2de1420b0ea8ccde4d6ea8
DucGiang-dev <giangnd.dev@gmail.com>
2026-07-22 06:21:23 +0700

    test: fix failing tests due to loading indicator change

--- test/features/dashboard/presentation/dashboard_screen_test.dart
+++ test/features/dashboard/presentation/dashboard_screen_test.dart
@@ -69,7 +69,7 @@ void main() {
 
       await tester.pump();
       
-      expect(find.byType(ClipOval), findsWidgets);
+      expect(find.text('Đang tải dữ liệu...'), findsOneWidget);
 
       transactionsCompleter.complete(<TransactionEntity>[]);
       categoriesCompleter.complete(<CategoryEntity>[]);
